d4cada76ff48754315330e91408270e32597c12b
andrey-lukyanov <aalukyanov@rambler.ru>
2020-07-30 21:15:29 +0300

    NSS model changed for NS, bugs corrected

--- new_thesis_library.py
+++ new_thesis_library.py
@@ -7,14 +7,13 @@ from scipy.optimize import LinearConstraint
 
 #/Users/andrey_lukyanov/Google_Drive/Studies/Year_4/Курсач/Coding/
 #C:/Users/1/Desktop/
-#C:/Users/aaluk/Documents/GitHub/
 
 #bonds_payments import
-bonds_payments = pd.read_csv('C:/Users/aaluk/Documents/GitHub/Comparing-numerical-methods-for-term-structure-fitting/Data/New_data/bonds_payments.csv', index_col = 0)
+bonds_payments = pd.read_csv('/Users/andrey_lukyanov/Google_Drive/Studies/Year_4/Курсач/Coding/Comparing-numerical-methods-for-term-structure-fitting/Data/New_data/bonds_payments.csv', index_col = 0)
 bonds_payments['Дата фактической выплаты'] = pd.to_datetime(bonds_payments['Дата фактической выплаты'], format='%Y-%m-%d')
 
 #bonds_prices import
-bonds_prices = pd.read_csv('C:/Users/aaluk/Documents/GitHub/Comparing-numerical-methods-for-term-structure-fitting/Data/New_data/bonds_prices.csv', index_col='TRADEDATE', parse_dates=True)
+bonds_prices = pd.read_csv('/Users/andrey_lukyanov/Google_Drive/Studies/Year_4/Курсач/Coding/Comparing-numerical-methods-for-term-structure-fitting/Data/New_data/bonds_prices.csv', index_col='TRADEDATE', parse_dates=True)
 
 #dates and trade_codes
 dates = bonds_prices.index
@@ -31,28 +30,37 @@ def nss(m, theta):
     return beta0 + beta1 * (1 - np.exp(- m / tau1)) / (m / tau1) + beta2 * ((1 - np.exp(- m / tau1)) / (m / tau1) - np.exp(- m / tau1)) + beta3 * ((1 - np.exp(- m / tau2)) / (m / tau2) - np.exp(- m / tau2))
 
 def reparameterized_nss(m, u):
-    return u[2] + (u[3] - u[2]) * (1 - np.exp(- m / u[0])) / (m / u[0]) + u[4] * ((1 - np.exp(- m / u[0])) / (m / u[0]) - np.exp(- m / u[0])) + u[5] * ((1 - np.exp(- m / u[1])) / (m / u[1]) - np.exp(- m / u[1]))    
+    return u[2] + (u[3] - u[2]) * (1 - np.exp(- m / u[0])) / (m / u[0]) + u[4] * ((1 - np.exp(- m / u[0])) / (m / u[0]) - np.exp(- m / u[0])) + u[5] * ((1 - np.exp(- m / u[1])) / (m / u[1]) - np.exp(- m / u[1]))
+
+def ns(m, theta):
+    tau = theta[0]
+    beta0 = theta[1]
+    beta1 = theta[2]
+    beta2 = theta[3]
+
+    return beta0 + beta1 * (1 - np.exp(- m / tau)) / (m / tau) + beta2 * ((1 - np.exp(- m / tau)) / (m / tau) - np.exp(- m / tau))
+
+def reparameterized_ns(m, u):
+    return u[1] + (u[2] - u[1]) * (1 - np.exp(- m / u[0])) / (m / u[0]) + u[3] * ((1 - np.exp(- m / u[0])) / (m / u[0]) - np.exp(- m / u[0]))
 
 #Add discounted by nss curve prices to a dataframe on a data. Works specifically for build_ss_loss_function 
 def discount(df, theta):
-    df['Discounted'] = (df['Сумма купона, RUB'] + df['Погашение номинала, RUB']) * np.exp(-nss(m = df['Дата фактической выплаты'], theta = theta) * df['Дата фактической выплаты'])
+    df['Discounted'] = (df['Сумма купона, RUB'] + df['Погашение номинала, RUB']) * np.exp(-ns(m = df['Дата фактической выплаты'], theta = theta) * df['Дата фактической выплаты'])
     
 def discount_rep(df, u):
-    df['Discounted'] = (df['Сумма купона, RUB'] + df['Погашение номинала, RUB']) * np.exp(-reparameterized_nss(m = df['Дата фактической выплаты'], u = u) * df['Дата фактической выплаты'])
+    df['Discounted'] = (df['Сумма купона, RUB'] + df['Погашение номинала, RUB']) * np.exp(-reparameterized_ns(m = df['Дата фактической выплаты'], u = u) * df['Дата фактической выплаты'])
     
 #loss function
 def build_ss_loss_function(date):
-    
+
     market_prices = bonds_prices[date:date].T
     market_prices.columns = ['Market prices']
-    market_prices.dropna(inplace=True)
-    
+    market_prices.dropna(inplace=True)    
+
     payments_on_date = bonds_payments[bonds_payments['Дата фактической выплаты'] >= date]
     payments_on_date = payments_on_date[payments_on_date['ISIN'].isin(market_prices.index)]
     
     def ss_loss_function(theta):
-        
-        C = np.ones(8) * 1000
         
         nonlocal payments_on_date, market_prices, date
     
@@ -64,7 +72,7 @@ def build_ss_loss_function(date):
         calc_prices = pd.DataFrame(calc_df.groupby('ISIN')['Discounted'].sum())
                 
         result_df = pd.concat([pd.DataFrame(calc_df.groupby('ISIN')['Discounted'].sum()), 
-                               corrected_market_prices], axis = 1)
+                               market_prices], axis = 1)
         
         #Sum of squares
         J = (((np.array(result_df['Discounted']) - np.array(result_df['Market prices']))/1000)**2).sum()
@@ -86,7 +94,7 @@ def tau_constraint(tau):
     
 def beta_0_constraint(beta_0):
     if beta_0 <= 0:
-        return beta_0**2
+        return beta_0
     else:
         return 0
        
@@ -102,7 +110,6 @@ def build_constrained_loss_function(date):
     market_prices.columns = ['Market prices']
     market_prices.dropna(inplace=True)
 
-    
     payments_on_date = bonds_payments[bonds_payments['Дата фактической выплаты'] >= date]
     payments_on_date = payments_on_date[payments_on_date['ISIN'].isin(market_prices.index)]
     
@@ -118,12 +125,12 @@ def build_constrained_loss_function(date):
         calc_prices = pd.DataFrame(calc_df.groupby('ISIN')['Discounted'].sum())
                 
         result_df = pd.concat([pd.DataFrame(calc_df.groupby('ISIN')['Discounted'].sum()), 
-                               corrected_market_prices], axis = 1)
+                               market_prices], axis = 1)
         
         #Sum of squares
         J = (((np.array(result_df['Discounted']) - np.array(result_df['Market prices']))/1000)**2).sum()
         
-        constraints = tau_constraint(theta[0])**2 + tau_constraint(theta[1])**2 + beta_0_constraint(theta[2])**2 + linear_constraint(theta[2], theta[3])**2
+        constraints = tau_constraint(theta[0])**2 + beta_0_constraint(theta[1])**2 + linear_constraint(theta[1], theta[2])**2
         
         return J + 1000 * constraints
     
@@ -136,15 +143,9 @@ def build_reparametarized_loss_function(date):
     market_prices = bonds_prices[date:date].T
     market_prices.columns = ['Market prices']
     market_prices.dropna(inplace=True)
-
     
     payments_on_date = bonds_payments[bonds_payments['Дата фактической выплаты'] >= date]
     payments_on_date = payments_on_date[payments_on_date['ISIN'].isin(market_prices.index)]
-    
-#    principals = pd.DataFrame(payments_on_date.groupby('ISIN')['Погашение номинала, RUB'].sum())
-#    
-#    #correction of amortizable bonds prices
-#    corrected_market_prices = np.multiply(market_prices, principals) / 100
     
     def loss_function(u):
         
@@ -171,15 +172,18 @@ reparameterized_loss_functions = [build_reparametarized_loss_function(dates[date
     
 #optimize on date by method with staring values
 def optimize_on_day_with_starting_values(date_number, method, theta0):
+    """
+    a copy should be supplied for theta0 fo L-BFGS-B and Powell, i.e. np.copy()
+    """
 
     if method == 'L-BFGS-B':
         
-        theta0[3] = theta0[3] + theta0[2]
+        theta0[2] = theta0[2] + theta0[1]
         
         loss_func = reparameterized_loss_functions[date_number]
         
-        bounds = Bounds([0, 0, 0, 0, -np.inf, -np.inf], 
-                        [30, 30, np.inf, np.inf, np.inf, np.inf])
+        bounds = Bounds([0, 0, 0, -np.inf], 
+                        [30, np.inf, np.inf, np.inf])
         
         start = dt.datetime.now()
     
@@ -189,37 +193,38 @@ def optimize_on_day_with_starting_values(date_number, method, theta0):
         execution_time = (dt.datetime.now() - start).total_seconds()
         
         theta = res.x
-        theta[3] = theta[3] - theta[2]
+        theta[2] = theta[2] - theta[1]
         
         return theta, execution_time
     
     elif method == 'Powell':
         
-        theta0[3] = theta0[3] + theta0[2]
+        theta0[2] = theta0[2] + theta0[1]
         
         loss_func = reparameterized_loss_functions[date_number]
         
-        bounds = ((0, 30), (0, 30), (0, np.inf), 
-                  (0, np.inf), (-np.inf, np.inf), (-np.inf, np.inf))
+        bounds = ((0, 30), (0, np.inf), 
+                  (0, np.inf), (-np.inf, np.inf))
         
         start = dt.datetime.now()
     
         res = minimize(loss_func, theta0, method=method,
                        options={'disp': False}, bounds=bounds)
-        
+
         execution_time = (dt.datetime.now() - start).total_seconds()
         
         theta = res.x
-        theta[3] = theta[3] - theta[2]
+        theta[2] = theta[2] - theta[1]
         
         return theta, execution_time        
         
-    elif method == 'trust-constr':            
+    elif method == 'trust-constr':      
+        
         loss_func = loss_functions[date_number]
         
-        bounds = Bounds([0, 0, 0, -np.inf, -np.inf, -np.inf], 
-                        [30, 30, np.inf, np.inf, np.inf, np.inf])
-        linear_constraint = LinearConstraint([[0, 0, 1, 1, 0, 0]], [0], [np.inf])
+        bounds = Bounds([0, 0, -np.inf, -np.inf], 
+                        [30, np.inf, np.inf, np.inf])
+        linear_constraint = LinearConstraint([[0, 1, 1, 0]], [0], [np.inf])
         
         res = minimize(loss_func, theta0, method=method, 
                        constraints=[linear_constraint],
@@ -227,6 +232,7 @@ def optimize_on_day_with_starting_values(date_number, method, theta0):
         return res.x, res.execution_time
            
     elif method == 'Nelder-Mead':
+        
         loss_func = constrained_loss_functions[date_number]
         
         start = dt.datetime.now()
